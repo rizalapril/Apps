57e2f492a8aaa1be794d1e71d8023698774fc926
Rizal <Rizal@i-dispatch.net>
2021-10-13 17:13:36 +0700

    create success fragment layout

---
+++ app/src/main/java/com/kaspin/view/fragment/SuccessFragment.kt
@@ -0,0 +1,21 @@
+package com.kaspin.view.fragment
+
+import android.view.View
+import com.kaspin.R
+import com.kaspin.base.BaseFragment
+
+class SuccessFragment: BaseFragment() {
+    override fun getLayoutResourceId(): Int = R.layout.fragment_success
+
+    override fun initView(parent: View) {
+
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun initObserver() {
+
+    }
+}
